80479e0670b5a15bfc9158c4637282f70e9eb577
ziaul888 <ziaul935@gmail.com>
2021-04-18 21:25:14 +0600

    navbar/about

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+// import pages
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+// import components
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route  path="/about"><About/></Route>
+        <Route path="/cocktail/:id" component={SingleCocktail}/>
+        <Route path="/error" component={Error}/>
+      </Switch>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section" >
+      <h1 className="section-title">About page</h1>
+      <p>So, RCB reclaim the top position for now and remain the only unbeaten side of the tournament. Is it finally their time to rule? Well, too early but they certainly have begun well, winning the first three games of the season for the first time ever certainly is a step in the right direction. KKR continue to have too many stars but no real clarity in the batting department. This defeat will hurt them, even as they try to come out of the shock that the loss to MI was. Plenty of problems to solve for them but it's still a long tournament. Speaking of the tournament, we're running out of time as the next game of the double-header is coming your way. So, switch tabs and enjoy.</p>
+    </section>
+  )
+}
+
+export default About
